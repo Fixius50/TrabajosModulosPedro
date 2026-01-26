f68a3c904e824f3e82567f47c4d71c58a9897b13
Fixius50 <robertomonederoalonso0@gmail.com>
2026-01-26 21:58:10 +0100

    asd

--- DesarrolloInterfaces/ServerMonitorApp/frontend/next.config.mjs
+++ DesarrolloInterfaces/ServerMonitorApp/frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/ServerMonitorApp/frontend', // Must match the folder structure in Vercel
+    images: {
+        unoptimized: true
+    }
+};
 
 export default nextConfig;
